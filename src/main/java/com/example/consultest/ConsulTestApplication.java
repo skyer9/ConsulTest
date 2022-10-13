0fd0da93a4dec1ff4b7760c0b2d29363e6570440
LeeSan <skyer9@gmail.com>
2022-10-13 21:18:46 +0900

    불필요한 import 제거

--- src/main/java/com/example/consultest/ConsulTestApplication.java
+++ src/main/java/com/example/consultest/ConsulTestApplication.java
@@ -1,17 +1,12 @@
 package com.example.consultest;
 
-import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @EnableFeignClients
