90f83e28b74368498e63adcb4fdfd2cae0a2e812
LeeSan <skyer9@gmail.com>
2022-10-17 13:20:55 +0900

    Add RequestParam

--- src/main/java/com/example/consultest/ConsulTestApplication.java
+++ src/main/java/com/example/consultest/ConsulTestApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -31,13 +33,12 @@ public class ConsulTestApplication {
 
     @RequestMapping("/use-feign")
     public String sendData() {
-        distributionClient.sendData("Hello World");
-        return "sent";
+        return distributionClient.sendData("Lee");
     }
 
-    @RequestMapping("/data")
-    public String test() {
-        return "Hello Test";
+    @GetMapping("/data")
+    public String test(@RequestParam(required = false) String query) {
+        return "Hello " + query;
     }
 
     @RequestMapping("/")
--- src/main/java/com/example/consultest/DistributionClient.java
+++ src/main/java/com/example/consultest/DistributionClient.java
@@ -1,12 +1,12 @@
 package com.example.consultest;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(value = "myprefix-myApp")
 public interface DistributionClient {
 
-    @RequestMapping(method = RequestMethod.POST, value = "/data", consumes = "application/json")
-    String sendData(String data);
+    @GetMapping(value = "/data", consumes = "application/json")
+    String sendData(@RequestParam(required = false) String query);
 }
